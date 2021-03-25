21adc985658c623cd72d85faa3eff98e46908f4d
Palgong-Mountain <70133222+PalgongMountain@users.noreply.github.com>
2021-03-25 20:41:01 +0900

    Update p110_5.py

--- python/p110_5.py
+++ python/p110_5.py
@@ -8,6 +8,6 @@ celsius = 섭씨
 """
 
 fahren = int(input("화씨 온도를 입력하시오: "))
-celsius = (fahren - 32.0) * (5/9)
+celsius = (fahren - 32.0) * 5/9
 
 print(f"화씨 {fahren}도는 섭씨 {round(celsius,2)}도에 해당합니다.")
