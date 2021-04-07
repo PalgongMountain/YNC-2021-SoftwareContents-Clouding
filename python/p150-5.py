d7366ae0b88f6ace74ffb5cba994944ed40a9540
Palgong-Mountain <70133222+PalgongMountain@users.noreply.github.com>
2021-04-07 11:28:36 +0900

    Add files via upload

--- python/p150-5.py
+++ python/p150-5.py
@@ -1,26 +1,47 @@
 """
-3개의 정수를 입력받아 가장 작은 값을 결정하는 프로그램
+3개의 정수를 입력받아 가장 작은 값을 결정하여 출력하는 프로그램
 
 학번 : 2105080
 이름 : 임지홍
 """
-import sys
+# import sys
 
-try :
-    print("(정수를 입력할 때, \"x,y,z\"형식으로 입력해주세요)")
-    x, y, z = eval(input("3개의 정수를 입력하세요 : "))
-except :
-    sys.exit("입력 형식이 잘못됐습니다. 각 정수를 ','기호로 구분해주세요.")
+# try :
+#     print("(정수를 입력할 때, \"x,y,z\"형식으로 입력해주세요)")
+#     x, y, z = eval(input("3개의 정수를 입력하세요 : "))
+# except :
+#     sys.exit("입력 형식이 잘못됐습니다. 각 정수를 ','기호로 구분해주세요.")
 
-if type(x) == type(0.1) or type(y) == type(0.1) or type(z) == type(0.1):
-    sys.exit("실수는 입력할 수 없습니다.")
+# if type(x) == type(0.1) or type(y) == type(0.1) or type(z) == type(0.1):
+#     sys.exit("실수는 입력할 수 없습니다.")
     
-if x < y :
-    min = x
+# if x < y :
+#     min = x
+# else :
+#     min = y
+
+# if min > z :
+#     min = z
+
+# print(f"제일 작은 정수는 {min}입니다.")
+
+########################################################################
+
+# a = int(input("첫번째 수를 입력하세요 : "))
+# b = int(input("두번째 수를 입력하세요 : "))
+# c = int(input("세번째 수를 입력하세요 : "))
+
+# d를 추가해서 4개를 동시에 비교하는 프로그램도 작성.
+a,b,c = eval(input("수를 입력하세요 : "))
+
+if a < b :
+    min = a;
 else :
-    min = y
+    min = b;
 
-if min > z :
-    min = z
+if min < c :
+    final_min = min;
+else :
+    fianl_min = c;
 
-print(f"제일 작은 정수는 {min}입니다.")
+print(f"입력값 {a},{b},{c} 중에서 가장 작은값은 {final_min} 입니다.")
--- python/p150-6.py
+++ python/p150-6.py
@@ -4,51 +4,84 @@
 학번 : 2105080
 이름 : 임지홍
 """
-import sys,random
+# import sys,random
 
-user = eval(input("선택하세요(1 : 가위, 2 : 바위, 3 : 보) "))
+# user = eval(input("선택하세요(1 : 가위, 2 : 바위, 3 : 보) "))
 
-isuserwin = False
-iscpuwin = False
-isdraw = False
+# isuserwin = False
+# iscpuwin = False
+# isdraw = False
 
-if type(user) == type(0.1):
-    sys.exit("실수는 입력할 수 없습니다.")
+# if type(user) == type(0.1):
+#     sys.exit("실수는 입력할 수 없습니다.")
     
-if(user <= 0 or user >= 4) :
-    sys.exit("수를 잘못 입력했습니다. 1에서 3 사이의 수를 입력해주세요")
+# if(user <= 0 or user >= 4) :
+#     sys.exit("수를 잘못 입력했습니다. 1에서 3 사이의 수를 입력해주세요")
     
-cpu = random.randint(1, 3)
+# cpu = random.randint(1, 3)
 
-if user == cpu:
-    isdraw = True
-elif user == 1 :
-    if cpu == 2 :
-        iscpuwin = True
-    elif cpu == 3 :
-        isuserwin = True
-elif user == 2 :
-    if cpu == 3 :
-        iscpuwin = True
-    elif cpu == 1 :
-        isuserwin = True
-elif user == 3 :
-    if cpu == 1 :
-        iscpuwin = True
-    elif cpu == 2 :
-        isuserwin = True
+# if user == cpu:
+#     isdraw = True
+# elif user == 1 :
+#     if cpu == 2 :
+#         iscpuwin = True
+#     elif cpu == 3 :
+#         isuserwin = True
+# elif user == 2 :
+#     if cpu == 3 :
+#         iscpuwin = True
+#     elif cpu == 1 :
+#         isuserwin = True
+# elif user == 3 :
+#     if cpu == 1 :
+#         iscpuwin = True
+#     elif cpu == 2 :
+#         isuserwin = True
 
-print(f"컴퓨터의 선택(1 : 가위, 2 : 바위, 3 : 보) {cpu}")
+# print(f"컴퓨터의 선택(1 : 가위, 2 : 바위, 3 : 보) {cpu}")
 
-if iscpuwin :
-    print("컴퓨터가 이겼음")
-elif isuserwin :
-    print("사용자가 이겼음")
-elif isdraw :
-    print("비겼음")
+# if iscpuwin :
+#     print("컴퓨터가 이겼음")
+# elif isuserwin :
+#     print("사용자가 이겼음")
+# elif isdraw :
+#     print("비겼음")
     
-        
+#############################################################
 
+import random;
+
+human = int(input("선택하시오 (1: 가위, 2: 바위, 3: 보) "));
+computer = random.randint(1, 3);
+
+if human == computer :
+    result = "비김";
+elif human == 1 and computer == 2 : 
+    result = "컴퓨터 승";
+elif human == 2 and computer == 3 : 
+    result = "컴퓨터 승"; 
+elif human == 3 and computer == 1 : 
+    result = "컴퓨터 승";      
+else :
+    result = "사람 승";
+
+if human == 1 :
+    user_choice = "가위";
+elif human == 2 :
+    user_choice = "바위";
+elif human == 3 :
+    user_choice = "보";
+    
+if computer == 1 :
+    com_choice = "가위";
+elif computer == 2 :
+    com_choice = "바위";
+elif computer == 3 :
+    com_choice = "보";
+    
+print(f"사용자 = {user_choice}, 컴퓨터 = {com_choice}");
+print(result);
+    
 
 
     
